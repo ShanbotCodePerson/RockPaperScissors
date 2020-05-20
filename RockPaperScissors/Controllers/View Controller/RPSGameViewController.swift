b9a4b430b957e0320499c7f4a8b2564af4c1d3fb
leoidiaz <zaid.leonardo@gmail.com>
2020-05-20 14:18:26 -0700

    Create Alert Controllers

--- RockPaperScissors/Controllers/View Controller/RPSGameViewController.swift
+++ RockPaperScissors/Controllers/View Controller/RPSGameViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class RPSGameViewController: UIViewController {
-
+    //MARK: - Outlets
     @IBOutlet weak var userHeart1: UIImageView!
     @IBOutlet weak var userHeart2: UIImageView!
     @IBOutlet weak var userHeart3: UIImageView!
@@ -24,19 +24,66 @@ class RPSGameViewController: UIViewController {
     @IBOutlet weak var userImageView: UIImageView!
     @IBOutlet weak var enemyImageView: UIImageView!
     
+    //MARK: - Properties
+    var enemyHearts = 3
+    var userHearts = 3
     
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
+    //MARK: - Actions
     @IBAction func waterButtonTapped(_ sender: Any) {
     }
+    
     @IBAction func fireButtonTapped(_ sender: Any) {
+        // If won
+        presentRoundAlertController(title: "You Won!")
+        // else if lost
+        presentRoundAlertController(title: "You Lost!")
+        // else tie
+        presentRoundAlertController(title: "TIE!")
     }
+    
     @IBAction func earthButtonTapped(_ sender: Any) {
     }
+    
     @IBAction func menuButtonTapped(_ sender: Any) {
+    }
+    
+    func resetBoard(){
+        waterButton.isHidden = false
+        earthButton.isHidden = false
+        fireButton.isHidden = false
+    }
+    
+    func newGame(){
+        var enemyHearts = 3
+        var userHearts = 3
+    }
+    
+    func presentRoundAlertController(title: String){
+        let alertController = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+        let continueAction = UIAlertAction(title: "Continue", style: .default) { (reset) in
+            
+        }
+        alertController.addAction(continueAction)
+        present(alertController, animated: true)
+    }
+    
+    private func presentFinalAlertController(title: String, message: String){
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let quitAction = UIAlertAction(title: "Quit", style: .destructive) { (quit) in
+            
+        }
+        let restartAction = UIAlertAction(title: "Restart", style: .default) { (_) in
+            self.newGame()
+        }
+        alertController.addAction(quitAction)
+        alertController.addAction(restartAction)
+        
+        present(alertController, animated: true)
     }
 }
