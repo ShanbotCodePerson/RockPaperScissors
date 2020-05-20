bde98be3b690848b15cf969147328416603127d6
leoidiaz <zaid.leonardo@gmail.com>
2020-05-20 16:04:12 -0700

    Add heart and reset logic to viewcontroller

--- RockPaperScissors/Controllers/View Controller/RPSGameViewController.swift
+++ RockPaperScissors/Controllers/View Controller/RPSGameViewController.swift
@@ -28,41 +28,61 @@ class RPSGameViewController: UIViewController {
     //MARK: - Properties
     var enemyHearts = 3
     var userHearts = 3
-    var enemyElements = [ #imageLiteral(resourceName: "fire"), #imageLiteral(resourceName: "water"), #imageLiteral(resourceName: "earth")]
-    
+    var elements = [ #imageLiteral(resourceName: "fire"), #imageLiteral(resourceName: "water"), #imageLiteral(resourceName: "earth")]
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        newGame()
     }
     
     //MARK: - Actions
     
     @IBAction func fireButtonTapped(_ sender: Any) {
         presentResult(elementValue: 0)
+        fireButton.isHidden = true
     }
     
     @IBAction func waterButtonTapped(_ sender: Any) {
         presentResult(elementValue: 1)
+        fireButton.isHidden = true
     }
     
     @IBAction func earthButtonTapped(_ sender: Any) {
         presentResult(elementValue: 2)
+        fireButton.isHidden = true
     }
-    
-    @IBAction func menuButtonTapped(_ sender: Any) {
-    }
+    // TODO: Delete
+//    @IBAction func menuButtonTapped(_ sender: Any) {
+//    }
     
     func presentResult(elementValue: Int){
         let randomPick = gameLogic.generateComputerMove()
         let result = gameLogic.getOutcome(of: elementValue, vs: randomPick)
-        enemyImageView.image = enemyElements[randomPick]
+        enemyImageView.image = elements[randomPick]
+        userImageView.image = elements[elementValue]
         switch result {
         case .win:
             presentRoundAlertController(title: "You Won!")
+            enemyHearts -= 1
+            var heartImages = [enemyHeart1.image, enemyHeart2.image, enemyHeart3.image]
+            heartImages[enemyHearts] = UIImage(named: "heart")
+            userHeart3.image = UIImage(named: "heart")
+            if enemyHearts == 0{
+                presentFinalAlertController(title: "YOU ARE THE WINNER", message: "How about another?")
+            }
+            resetBoard()
         case .lose:
             presentRoundAlertController(title: "You Lost!")
+            userHearts -= 1
+            var heartImages = [userHeart1.image, userHeart2.image, userHeart3.image]
+            heartImages[enemyHearts] = UIImage(named: "heart")
+            if userHearts == 0{
+                presentFinalAlertController(title: "YOU ARE THE LOSER", message: "Want to play again loser?")
+            }
+            resetBoard()
         case .tie:
             presentRoundAlertController(title: "TIE!")
+            resetBoard()
         }
     }
     
@@ -70,17 +90,22 @@ class RPSGameViewController: UIViewController {
         waterButton.isHidden = false
         earthButton.isHidden = false
         fireButton.isHidden = false
+        userImageView.image = UIImage(named: "questionmark.circle")
+        enemyImageView.image = UIImage(named: "questionmark.circle")
     }
     
     func newGame(){
         enemyHearts = 3
         userHearts = 3
+        resetBoard()
+        let fullHearts = [enemyHeart1, enemyHeart2, enemyHeart3, userHeart1, userHeart2, userHeart3]
+        fullHearts.forEach({$0?.image = UIImage(named: "heart.filled")})
     }
     
     func presentRoundAlertController(title: String){
         let alertController = UIAlertController(title: title, message: nil, preferredStyle: .alert)
-        let continueAction = UIAlertAction(title: "Continue", style: .default) { (reset) in
-            
+        let continueAction = UIAlertAction(title: "Continue", style: .default) { [weak self] (_) in
+            self?.resetBoard()
         }
         alertController.addAction(continueAction)
         present(alertController, animated: true)
@@ -90,10 +115,10 @@ class RPSGameViewController: UIViewController {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
         let quitAction = UIAlertAction(title: "Quit", style: .destructive) { (quit) in
-            
+            // TODO: Navigation controller pop off
         }
-        let restartAction = UIAlertAction(title: "Restart", style: .default) { (_) in
-            self.newGame()
+        let restartAction = UIAlertAction(title: "Restart", style: .default) { [weak self] (_) in
+            self?.newGame()
         }
         alertController.addAction(quitAction)
         alertController.addAction(restartAction)
